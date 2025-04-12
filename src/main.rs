779acefc956c4ae8af6c620d8f77fc51a7eccca8
wenwang.wyp <wenwang.wyp@antgroup.com>
2025-04-12 23:26:05 +0800

    系统初始化mysql的yaml读取功能

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use quant_engine::config;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 初始化连接池
+    let sys_config = config::sys_config::GLOBAL_CONFIG.lock().unwrap();
+    // 初始化连接池
+    config::db_config::init_global_pool(&sys_config.database_url).await?;
+
+    // 启动web服务...
+    Ok(())
 }
